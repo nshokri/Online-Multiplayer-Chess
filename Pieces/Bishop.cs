fee8ac15907697892e8baa84b1ee4354a9393b30
Nick Shokri <nshokri123@gmail.com>
2021-02-05 17:01:00 -0800

    Uploading Pieces classes

---
+++ Pieces/Bishop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_Server.Pieces
+{
+    public class Bishop : ChessPiece
+    {
+        public Bishop(int x, int y, int player) : base(x, y, player)
+        {
+
+        }
+    }
+}
